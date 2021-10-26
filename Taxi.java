a7edad0e0d4aa7022d78d39cf80df253781dd3cf
tloy-x <theo94x@gmail.com>
2021-10-25 23:11:24 -0400

    Mooooooore docs

--- Taxi.java
+++ Taxi.java
@@ -2,34 +2,70 @@ public class Taxi extends Automobile {
     private Person driver;
     private String ID;  
 
+    /**
+     * Constructor for the Taxi class
+     * @param owner
+     * @param make
+     * @param model
+     * @param year
+     * @param mileage
+     * @param numPassengers
+     * @param isSUV
+     * @param driver
+     * @param ID
+     */
     public Taxi(Person owner, String make, String model, int year, int mileage, int numPassengers, boolean isSUV, Person driver, String ID) {
         super(owner, make, model, year, mileage, numPassengers, isSUV);
         this.driver = driver;
         this.ID = ID;
     }
 
+    /**
+     * Getter for driver
+     * @return a Person driver 
+     */
     public Person getDriver() {
         return this.driver;
     }
     
+    /**
+     * Getter for ID 
+     * @return a String ID 
+     */
     public String getID() {
         return this.ID;
     }
 
+    /**
+     * Setter for driver 
+     * @param driver driver of the Taxi in Person 
+     */
     public void setDriver(Person driver) {
         this.driver = driver;
     }
 
+    /**
+     * Setter for ID 
+     * @param ID ID of the Taxi in String 
+     */
     public void setID(String ID) {
         this.ID = ID;
     }
 
     @Override
+    /**
+     * Overridden toString() method for Taxi
+     * @return a String containing formatted output 
+     */
     public String toString() {
         return String.format("%s\nDriver: %s ID#%s", super.toString(), this.getDriver(), this.getID());
     }
 
     @Override
+    /**
+     * Overridden equals() method for Taxi
+     * @return true if all fields are the same, false otherwise
+     */
     public boolean equals(Object o) {
         if (o.getClass() != this.getClass())
             return false;
--- VehicleDriver.java
+++ VehicleDriver.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 
 public class VehicleDriver {
-
     public static void main(String[] args) throws IOException{
         Scanner sc = new Scanner(System.in);
 
